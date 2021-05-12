52667aad22e723670671d5f063737847b3df4867
Anthony Howard-Barnhardt, Sr <trackinghb@gmail.com>
2021-05-12 05:43:25 -0700

    started on html and js objects

---
+++ js/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const locationSeattle - {
+  city: 'Seattle',
+  minCust: '23',
+  maxCust: '65',
+  avgCookieSale: '6.3'
+  cookiePerHour: [],
+  hours: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'],
+
+  setCookiePerHour(){
+    this.cookiePerHour.push(Math.cell(randomCustomers(this.minCust,this.maxCust) * this.avg))
+  }
+}
+
+const locationTokyo - {
+  city: 'Tokyo',
+  minCust: '3',
+  maxCust: '24',
+  avgCookieSale: '1.2'
+  cookiePerHour: [],
+  hours: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'],
+
+  setCookiePerHour(){
+    this.cookiePerHour.push(Math.cell(randomCustomers(this.minCust,this.maxCust) * this.avg))
+  }
+}
+
+const locationDubai - {
+  city: 'Dubai',
+  minCust: '11',
+  maxCust: '38',
+  avgCookieSale: '3.7'
+  cookiePerHour: [],
+  hours: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'],
+
+  setCookiePerHour(){
+    this.cookiePerHour.push(Math.cell(randomCustomers(this.minCust,this.maxCust) * this.avg))
+  }
+}
+
+const locationParis - {
+  city: 'Paris',
+  minCust: '20',
+  maxCust: '38',
+  avgCookieSale: '2.3'
+  cookiePerHour: [],
+  hours: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'],
+
+  setCookiePerHour(){
+    this.cookiePerHour.push(Math.cell(randomCustomers(this.minCust,this.maxCust) * this.avg))
+  }
+}
+
+const locationLima - {
+  city: 'Lima',
+  minCust: '2',
+  maxCust: '16',
+  avgCookieSale: '4.6'
+  cookiePerHour: [],
+  hours: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'],
+
+  setCookiePerHour(){
+    this.cookiePerHour.push(Math.cell(randomCustomers(this.minCust,this.maxCust) * this.avg))
+  }
+}
+
